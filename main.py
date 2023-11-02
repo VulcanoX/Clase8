49cc82a1bfebc0b4f76cbe2d35ec987be9291daf
Santiago <santiagogiraldosexto@gmail.com>
2023-11-02 15:55:45 -0500

    crea varias listas y muestra su tamaño. crear el .gitignore

---
+++ main.py
@@ -0,0 +1,61 @@
+
+
+
+'''
+Hasta ahora hemos trabajo
+con varibales
+que permiten almacenar un unico
+valor
+'''
+
+edad = 12
+
+altura = 1.79
+
+nombre = "Juan"
+
+estado = True
+
+'''
+En python podemos usar
+variables que
+almacenan una coleccion
+de datos y luego
+acceder a ella usando
+un subindice
+'''
+
+#Lista de enteros
+lista1 = [10, 5, 3, 9]
+
+#Lista de decimales
+lista2 = [1.78, 2.66, 1.55, 89.4]
+
+#Lista de String
+lista3 = ["Lunes", "Martes", "Miercoles"]
+
+#Lista de elementos de distinto tipo
+
+lista4 = ["Juan", 45, 1.92, False]
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    #Cantidad de elementos de la lista
+    print(len(lista1))
+    print(len(lista2))
+    print(len(lista3))
+    print(len(lista4))
+
+    print()
+    print(lista1)
+
+
+
+
+
